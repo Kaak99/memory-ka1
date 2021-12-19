20c3192df59848632c59ef8a70fb1b370fe50cad
Kaak <cours.kaak@gmail.com>
2021-12-19 20:16:03 +0100

    minor
    css: :hover ...

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ console.log("******** start ********");
 
 //const allCards=[];
 
+let audioCoin = new Audio('./audio/coin.mp3');
+let audioDog = new Audio('./audio/dog.mp3');
+let audioYeah = new Audio("./audio/yeah.mp3");
+
 let cardIsReturned = false;
 let firstCard, secondCard;
 let clickIsLocked = false;
@@ -30,7 +34,7 @@ function start() {
   
   allBoardHTML.addEventListener("click", function () {
     allBoardHTML.style.display = "none";
-    new Audio('./audio/coin.mp3').play()
+    audioCoin.play()
     scoreHTML.textContent = "Dernier score : " + clickCounter + " clicks !";
     clickCounter = 0; //compte les click
     matchCounter = 0; //compte les match
@@ -75,7 +79,7 @@ function checkSameCards() {
     firstCard.removeEventListener("click", cardReturn);
     secondCard.removeEventListener("click", cardReturn);
     matchCounter++;
-    new Audio("./audio/dog.mp3").play();
+    audioDog.play();
     if (matchCounter === 8) {
       gameOver();
     }
@@ -93,9 +97,8 @@ function gameOver() {
   console.log("-----gameOver------");
   //console.log("fonction gameover");
   //console.log(clickCounter);
-  new Audio("./audio/yeah.mp3").play();
+  audioYeah.play();
   //alert("Succès en " +clickCounter +" coups !")
-  //setTimeout (() => {
   //allBoardHTML.style.animation = 'grossit 3s';
   allBoardHTML.style.display = "flex";
   messageBoardHTML.innerText = `  🤩 Bravo !!! 🤩 
@@ -104,7 +107,6 @@ function gameOver() {
 
       Cliquez pour recommencer ! `;
   //pas de retour à la ligne avec textContent(meme avec r\n \n ou \u00a0)
-  //}, 5000 )
   preStart();
 }
 
@@ -126,114 +128,20 @@ function randomOrder() {
   });
 }
 
-// function watchCards(){
 
-// }
-
-// const scoreHTML = document.querySelector('.score');
-// scoreHTML.textContent = "Dernier score : " + clickCounter + " coups !";
-
-//start();//aleatoire, vire board et affiche allcards (face cachee)
-//surveille click cartes
-//si game over : affiche board et cache cartes
-//watchCardsclick();
-
-//  start();
-
-// function start(){
-//   //console.log("fonction start");
-//  const allCards = document.querySelectorAll('.cardsExt');
-  //console.log(allCards);
- //zik, modal score, restart
-//   randomOrder();
-
-// }
-
-//  // ************************fonctions ********************* //
-
-//  function start(){
-//console.log("fonction start");
-//   randomOrder();
-//   allCards = document.querySelectorAll('.cardsExt');
-  //console.log(allCards);
-
-//   mainGameInt.style.visibility = 'visible';
-//   new Audio('./audio/coin.mp3').play()
-//  }
-
-// function cardReturn(){
-//   if(clickIsLocked) return;//empeche select + de 2 cartes
-//   clickCounter++;//sinon on compte un clic
- //console.log(clickCounter);
- //console.log(this.childNodes[1]);
-//   this.childNodes[1].classList.toggle('active');
-//   if(!cardIsReturned){
-//     cardIsReturned = true;
-//     firstCard = this;
-//     return;
-//   }
-//   cardIsReturned = false;
-//   secondCard = this;
-   //console.log(firstCard, secondCard);
-   //checkSameCards();
- //console.log(firstCard.id);
-//console.log(secondCard.id);
-//console.log(firstCard.style.order);
-//console.log(secondCard.style.order);
-//   if ( firstCard.id !== secondCard.id ){
-//     checkSameCards();
-//   }
-// }
-
-// function checkSameCards(){
-//   if (firstCard.getAttribute('data-attr') === secondCard.getAttribute('data-attr')) {
-//     firstCard.removeEventListener('click', cardReturn);
-//     secondCard.removeEventListener('click', cardReturn);
-//     matchCounter++ ;
-//     new Audio('./audio/dog.mp3').play()
-//     if (matchCounter === 8) {
-//       gameOver()
-//     }
-//   }
-//   else{
-//     clickIsLocked = true ;
-//     setTimeout (() => {
-//       firstCard.childNodes[1].classList.remove('active');
-//       secondCard.childNodes[1].classList.remove('active');
-//       clickIsLocked = false ;
-//     }, 1500 )
-//   }
-// }
-
-// function gameOver(){
-//   //console.log("fonction gameover");
-//   //console.log(clickCounter);
-//   new Audio('./audio/yeah.mp3').play()
-//   //alert("Succès en " +clickCounter +" coups !")
-//   const scoreHTML = document.querySelector('.score');
-//   scoreHTML.textContent = "Dernier score : " + clickCounter + " coups !";
-//   const mainBoard = document.querySelector('.mainBoard');
-//   mainBoard.textContent = `  🤩 Bravo !!! 🤩
-//   ✨Vous avez réussi en ${clickCounter} coups !✨
-//   Cliquez pour recommencer ! `;
-//   // document.location.reload();
-//   //zik, modal score, restart
-//   //start();
-// }
-
-// function randomOrder(){
-//   //console.log("fonction randomOrder");
-//   allCards.forEach(card => {
-//     let randomPosition = Math.floor(Math.random() * 12);
-//     //console.log(randomPosition);
-//     card.style.order = randomPosition ;
-//     //console.log(allCards);
-//   })
-// }
-
-//   card.addEventListener('click', returnCard)
-// });
-
-// function returnCard() {
-//   //console.log(this.childNodes);
-// }
+/*
+plutot que 
+new Audio("./audio/yeah.mp3").play(); 
+new Audio("./audio/dog.mp3").play();
+new Audio("./audio/coin.mp3").play();
+... etc (pour chaque son)
+faire
+let audioYeah = new Audio('./audio/yeah.mp3');
+let audioCoin = new Audio('./audio/coin.mp3');
+let audioDog = new Audio('./audio/dog.mp3');
+...
+puis audioYeah.play();
+puis audioCoin.play();
+puis audioDog.play();
+...
+*/
